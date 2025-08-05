9ddf6d32af8521c3f0c7a127a7ee509d6b040c70
Nils GB <nilsgrossebeikel@gmail.com>
2025-08-05 19:36:59 +0200

    added express routing for every site

--- routes/pages.js
+++ routes/pages.js
@@ -5,7 +5,28 @@ const db = require('../db/database');
 
 // Standardseite
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/station_administration.html'));
 });
+
+// Routen-Verwaltungsseite
+router.get('/routen', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/route_administration.html'));
+});
+
+// Stationen-Verwaltungsseite
+router.get('/stationen', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/station_administration.html'));
+});
+
+// Impressumsseite
+router.get('/impressum', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/impressum.html'));
+});
+
+// Datenschutzseite
+router.get('/datenschutz', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/datenschutz.html'));
+});
+
 
 module.exports = router;
