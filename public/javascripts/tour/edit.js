e1f28a7125e0712cc3e4475f08a1c520b71f9755
Nils GB <nilsgrossebeikel@gmail.com>
2025-09-02 21:27:31 +0200

    adjust structure for tour edit

--- public/javascripts/tour/edit.js
+++ public/javascripts/tour/edit.js
@@ -1,1 +1,21 @@
 "use strict"
+
+const editTourMap = L.map('edit-tour-map').setView([51, 10], 6);
+
+/**
+ * Initialisiert die Karte beim Laden der Webseite.
+ */
+function initMapEditTour() {
+    // OSM
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(editTourMap);
+}
+
+// Warte bis das HTML-Element existiert
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("edit-tour-map")) {
+    initMapEditTour();
+  }
+})
